1a44fc96211fe0aeb6aa9a98d59713b3d1face93
zhuNodeJs <1749238156@qq.com>
2018-05-07 20:50:08 +0800

    commit

--- meEle/src/components/goods/goods.vue
+++ meEle/src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="(item, index) in goods" :key="index" @click="menuClick(index, $event)"
+        <li class="muItem" v-for="(item, index) in goods" :key="index" @click="menuClick(index, $event)"
           :class="index === menuCurrentIndex ? 'menu-item-selected': 'menu-item'"
         >
           <span class="text">
