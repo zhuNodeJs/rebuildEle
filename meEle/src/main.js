b0abdd90f72ade8eee19135a2d1abea9b212c797
zhuNodeJs <1749238156@qq.com>
2018-05-18 15:53:20 +0800

    commit

--- meEle/src/main.js
+++ meEle/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import vueTap from 'v-tap'
+import fastclick from 'fastclick'
+
 
 Vue.config.productionTip = false
 
